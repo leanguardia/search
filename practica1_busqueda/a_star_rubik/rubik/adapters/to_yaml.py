d3f4f42dd982f38098b28cb75acbac687a27fd5a
DayanAguilar <dayan.aguilar.alcocer@gmail.com>
2024-05-01 10:25:58 -0400

    ToYAML class added

---
+++ practica1_busqueda/a_star_rubik/rubik/adapters/to_yaml.py
@@ -0,0 +1,30 @@
+from adapters.adapter import CubeAdapter
+COLORS = {
+    "W": "white",
+    "O": "orange",
+    "G": "green",
+    "R": "red",
+    "B": "blue",
+    "Y": "yellow"
+}
+
+class ToYAML(CubeAdapter):
+    
+    def adapt(self):
+        yaml_string = ""
+        for face, rows in self.face_dict.items():
+            yaml_string += f"{self._get_face_name(face)}:\n"
+            for row in rows:
+                yaml_string += "    " + " ".join(COLORS[color] for color in row) + "\n"
+        return yaml_string
+
+    def _get_face_name(self, face):
+        face_names = {
+            'U': 'up',
+            'F': 'front',
+            'L': 'left',
+            'R': 'right',
+            'B': 'back',
+            'D': 'down'
+        }
+        return face_names[face]
---
+++ practica1_busqueda/a_star_rubik/rubik/main_yaml.py
@@ -0,0 +1,37 @@
+import os
+from cube_loader import CubeLoader
+from adapters.to_yaml import ToYAML
+def load_file_from_user_input():
+    file_name = input('Nombre de archivo: /cubes/')
+    file_path = "cubes/" + file_name
+    if file_name[-4:] != '.txt':
+        file_path += '.txt'
+    print("Loading cube from file: " + file_path)
+
+    loader = CubeLoader()
+    print(loader.load(file_path))
+
+def load_sample_cubes_file_names():
+    file_names = os.listdir('cubes')
+    # filter the ones starting with cube
+    file_names = [f for f in file_names if f[:4] == 'cube']
+    return file_names
+
+if __name__ == '__main__':
+    # load_file_from_user_input()
+
+    cube_file_names = load_sample_cubes_file_names()
+    print(len(cube_file_names), "files found in /cubes/ folder")
+
+    # NOTE: this folder must exist
+    folder_output = 'cubes/yaml/'
+
+    for file_name in cube_file_names:
+        loader = CubeLoader()
+        print("Loading cube", file_name)
+        face_dict = loader.load('cubes/' + file_name)
+        adapter = ToYAML(face_dict)
+        yaml_cube = adapter.adapt()
+        with open(folder_output + file_name, 'w') as f:
+             f.write(yaml_cube)
+        print("Cube", file_name, "adapted to new format")
